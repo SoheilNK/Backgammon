044f20491c9ef59978f1f269ae4b90de0c5877f6
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-03-03 16:04:01 -0330

    checkers design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-
+// const clr: Text = ""
 function App() {
   return (
-    null
+    <Checker />
   )
 }
 
 export default App
+let imgUrl = ""
+let clr = ""
+
+function Checker() {
+  if (true) {
+    imgUrl = "Checker-W.jpg";
+  }
+  
+  return (
+    <div>
+      <img src={imgUrl} />
+    </div>
+  );
+}
