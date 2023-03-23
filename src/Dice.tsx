55becf0aca8086490c76aa5f0b6663211dd5e239
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-03-23 09:49:15 -0230

    all states added

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -3,30 +3,36 @@ interface DiceProps {
   currentDiceRoll: TdiceRoll;
   callback: Function;
   disabled: boolean;
-  setPlayer: Function;
+  setPlayer: Function; //test
+  setPlayerWon: Function;//test
 }
 
 export default function Dice({
-    currentDiceRoll,
-    callback,
-    disabled,
-    setPlayer,
+  currentDiceRoll,
+  callback,
+  disabled,
+  setPlayer, //test
+  setPlayerWon, //test
 }: DiceProps) {
-    return (
-        <div>
-            <h2>{currentDiceRoll}</h2>
-            <button type="button" disabled={disabled} onClick={() => {
-                currentDiceRoll = [
-                  Math.floor(Math.random() * 5 + 1),
-                  Math.floor(Math.random() * 5 + 1),
-                ] as TdiceRoll;
-                callback(currentDiceRoll)
-                setPlayer(PlayerNames.white) //test
-                // console.log(currentDiceRoll);
-            }}>
-                Roll Dice
-            </button>
-        </div>
-    );
-    
+  return (
+    <div>
+      <h2>{currentDiceRoll}</h2>
+      <button
+        type="button"
+        disabled={disabled}
+        onClick={() => {
+          currentDiceRoll = [
+            Math.floor(Math.random() * 5 + 1),
+            Math.floor(Math.random() * 5 + 1),
+          ] as TdiceRoll;
+          callback(currentDiceRoll);
+          setPlayer(PlayerNames.black); //test
+          setPlayerWon(false); //test
+          // console.log(currentDiceRoll);
+        }}
+      >
+        Roll Dice
+      </button>
+    </div>
+  );
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,3 @@
-import { getInitialState } from "@dnd-kit/core/dist/store";
 import { useState } from "react";
 import { Board } from "./Board";
 import Dice from "./Dice";
@@ -6,27 +5,38 @@ import Dice from "./Dice";
 export type Color = "White" | "Black";
 export type Direction = "rtl" | "ltr";
 export type TdiceRoll = [1 | 2 | 3 | 4 | 5 | 6, 1 | 2 | 3 | 4 | 5 | 6];
-export enum PlayerNames { white = "Player1", black = "Player2"}
+export enum PlayerNames { white = "Player1", black = "Player2" }
+export interface whitePlayer {
+  name: PlayerNames.white;
+  won: boolean;
+}
+export interface blackPlayer {
+  name: PlayerNames.black;
+  won: boolean;
+}
 interface GameProps {
   currentDiceRoll: TdiceRoll;
   currentBoardState: Color[][];
   // currentPlayer: enum
   currentPlayer: PlayerNames;
+  playerWon: boolean
 }
 
-function Game({ currentDiceRoll, currentBoardState, currentPlayer }: GameProps) {
+function Game({ currentDiceRoll, currentBoardState, currentPlayer, playerWon }: GameProps) {
   const [diceRoll, setDiceRoll] = useState(currentDiceRoll);
   const [boardState, setBoardState] = useState(currentBoardState);
   const [player, setPlayer] = useState(currentPlayer);
-
+  const [won, setPlayerWon]= useState(playerWon)
   console.log(diceRoll);
-  console.log(player);
+  console.log(player);//test
+  console.log(won);//test
 
   return (
     <div>
       <Board setBoardState={setBoardState} currentState={initialState} />
       <Dice
         setPlayer={setPlayer} //test
+        setPlayerWon={setPlayerWon} //test
         currentDiceRoll={diceRoll}
         callback={setDiceRoll}
         disabled={false}
