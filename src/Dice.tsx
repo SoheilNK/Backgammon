6ff965438d85344dcc70835cc7cd7ea19f4db511
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-04-06 22:48:54 -0230

    some design

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -34,44 +34,48 @@ export default function Dice({
   });
 
   return (
-    <div className="players">
-      <h2 className="player">{newDiceRoll}</h2>
-      <button
-        id="roll"
-        className={"player " + btnClass} 
-        onMouseDown={() => setIsPressed(true)}
-        onMouseUp={() => setIsPressed(false)}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        type="button"
-        disabled={diceDisabled}
-        onClick={() => {
-          onDouble(false);
-          onDiceDisabled(true);
-          newDiceRoll = [
-            Math.round(Math.random() * 5 + 1),
-            Math.round(Math.random() * 5 + 1),
-          ] as TdiceRoll;
-          /************************************************************ */
-          // currentDiceRoll = [2, 2] as TdiceRoll; //for testing double
-          onRoll(newDiceRoll);
-          onMessage(currentPlayer + " move a checker");
-
-          if (newDiceRoll[0] === newDiceRoll[1]) {
-            //if the dice roll is a double, the player can move twice
-            onDouble(true);
-            onDoubleLeft(4);
-            onMessage(
-              currentPlayer + " rolled a double, you have four moves left"
-            );
-          }
-        }}
-      >
-        Roll Dice
-      </button>
+    <div className="actionPanel">
       <h3 id="alert" className="player">
         {message}
       </h3>
+
+      <div className="players">
+        <h2 className="dice">{newDiceRoll}</h2>
+        <button
+          id="roll"
+          className="dice" //btnClass
+          onMouseDown={() => setIsPressed(true)}
+          onMouseUp={() => setIsPressed(false)}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          type="button"
+          disabled={diceDisabled}
+          onClick={() => {
+            onDouble(false);
+            onDiceDisabled(true);
+            newDiceRoll = [
+              Math.round(Math.random() * 5 + 1),
+              Math.round(Math.random() * 5 + 1),
+            ] as TdiceRoll;
+            /************************************************************ */
+            // currentDiceRoll = [2, 2] as TdiceRoll; //for testing double
+            onRoll(newDiceRoll);
+            onMessage(currentPlayer + " move a checker");
+
+            if (newDiceRoll[0] === newDiceRoll[1]) {
+              //if the dice roll is a double, the player can move twice
+              onDouble(true);
+              onDoubleLeft(4);
+              onMessage(
+                currentPlayer + " rolled a double, you have four moves left"
+              );
+            }
+          }}
+        >
+          <span>Roll Dice</span>
+        </button>
+      </div>
     </div>
   );
 }
+
