443c0550dbd411b0681f130ee1e890e23f185f34
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-04-22 14:22:35 -0230

    finally a dice with 6 numbers

--- src/Dice3D.tsx
+++ src/Dice3D.tsx
@@ -1,79 +1,74 @@
-import React, { Suspense } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls, useTexture } from "@react-three/drei";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
-//--------------------------------------------------------------
 import * as THREE from "three";
-import  { useRef, useState } from "react";
-import {  useFrame, ThreeElements } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import {
+  Canvas,
+  useFrame,
+  ThreeElements,
+  useLoader,
+  Color,
+} from "@react-three/fiber";
+import { TextureLoader } from "three";
 
-function Box(props: ThreeElements["mesh"]) {
+function Box2(props: ThreeElements["mesh"]) {
   const mesh = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += delta));
+
+  // all texture
+  const frontTexture = useLoader(TextureLoader, "textures/dice_2.jpg");
+  const backTexture = useLoader(TextureLoader, "textures/dice_4.jpg");
+  const topTexture = useLoader(TextureLoader, "textures/dice_1.jpg");
+  const bottomTexture = useLoader(TextureLoader, "textures/dice_6.jpg");
+  const leftTexture = useLoader(TextureLoader, "textures/dice_3.jpg");
+  const rightTexture = useLoader(TextureLoader, "textures/dice_5.jpg");
+
+  // let colors = ['red','green','blue','yellow','purple','orange'];
+  let colors = ["white", "white", "white", "white", "white", "white"];
+  let textures = [
+    frontTexture,
+    backTexture,
+    topTexture,
+    bottomTexture,
+    leftTexture,
+    rightTexture,
+  ];
+  let materials = [];
+  for (let i = 0; i < 6; i++) {
+    materials.push(
+      new THREE.MeshStandardMaterial({
+        map: textures[i],
+        color: hovered ? "hotpink" : "#e3dac9",
+      })
+    );
+  }
+
+  useFrame(
+    (_state, _delta) =>
+      (mesh.current.rotation.x = mesh.current.rotation.y += 0.01)
+  );
+
   return (
     <mesh
       {...props}
       ref={mesh}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      onClick={(_event) => setActive(!active)}
+      onPointerOver={(_event) => setHover(true)}
+      onPointerOut={(_event) => setHover(false)}
+      material={materials}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
-  );
-}
-
-// All textures are CC0 textures from: https://cc0textures.com/
-const name = (type: string) => `PavingStones092_1K_${type}.jpg`;
-
-function Scene() {
-  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
-    TextureLoader,
-    [
-      name("Color"),
-      name("Displacement"),
-      name("Normal"),
-      name("Roughness"),
-      name("AmbientOcclusion"),
-    ]
-  );
-
-  return (
-    <>
-      <ambientLight intensity={0.2} />
-      <directionalLight />
-      <mesh>
-        {/* Width and height segments for displacementMap */}
-        <sphereBufferGeometry args={[1, 100, 100]} />
-        <meshStandardMaterial
-          displacementScale={0.3}
-          map={colorMap}
-          displacementMap={displacementMap}
-          normalMap={normalMap}
-          roughnessMap={roughnessMap}
-          aoMap={aoMap}
-        />
-      </mesh>
-      <Box position={[-3.5, 0, 0]} />
-      <Box position={[3.5, 0, 0]} />
-    </>
   );
 }
 
 export default function Dice3D() {
   return (
-    <div className="h-80">
-      <Canvas>
-
-        <Suspense fallback={null}>
-          <Scene />
-          <OrbitControls autoRotate />
-        </Suspense>
-      </Canvas>
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[1, 1, 1]} />
+      <Box2 position={[-1.2, 0, 0]} />
+      <Box2 position={[1.2, 0, 0]} />
+    </Canvas>
   );
 }
