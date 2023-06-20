9ad1ec7d957954c75aff5f0ad2960f91a5d82222
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-06-20 14:30:33 -0230

    web socket chat test working

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,42 +1,157 @@
-import WebSocket, { WebSocketServer } from "ws";
+import { Component, ChangeEvent } from "react";
+import { w3cwebsocket as W3CWebSocket, IMessageEvent } from "websocket";
+import { Card, Avatar, Input, Typography } from "antd";
 
-//create a websocket client and connect to the server for chat
-const socket = new WebSocket("ws://localhost:8080");
+const { Search } = Input;
+const { Text } = Typography;
+const { Meta } = Card;
 
-const Chat: React.FC = () => {
+const client = new W3CWebSocket("ws://localhost:8001");
 
-    return (
-        <div>
-            <h1>Chat</h1>
-            <div id="messages"></div>
-            <form name="publish">
-                <input name="message" type="text" />
-                <input type="submit" value="Send" />
-            </form>
-        </div>
+interface Message {
+  msg: string;
+  user: string;
+}
+
+interface DataFromServer {
+  type: string;
+  msg: string;
+  user: string;
+}
+
+interface ChatState {
+  userName: string;
+  isLoggedIn: boolean;
+  messages: Message[];
+  searchVal: string;
+}
+
+export default class Chat extends Component<{}, ChatState> {
+  state: ChatState = {
+    userName: "",
+    isLoggedIn: false,
+    messages: [],
+    searchVal: "",
+  };
+
+  onButtonClicked = (value: string): void => {
+    client.send(
+      JSON.stringify({
+        type: "message",
+        msg: value,
+        user: this.state.userName,
+      })
     );
+    this.setState({ searchVal: "" });
+  };
+
+  componentDidMount(): void {
+    client.onopen = (): void => {
+      console.log("WebSocket Client Connected");
+    };
+
+    client.onmessage = (message: IMessageEvent): void => {
+      const dataFromServer: DataFromServer = JSON.parse(
+        message.data.toString()
+      );
+      console.log("got reply! ", dataFromServer);
+      if (dataFromServer.type === "message") {
+        this.setState((prevState) => ({
+          messages: [
+            ...prevState.messages,
+            {
+              msg: dataFromServer.msg,
+              user: dataFromServer.user,
+            },
+          ],
+        }));
+      }
+    };
+  }
+
+  handleSearchChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ searchVal: e.target.value });
+  };
+
+  handleSearchSubmit = (value: string): void => {
+    this.onButtonClicked(value);
+  };
+
+  handleLoginSubmit = (value: string): void => {
+    this.setState({ isLoggedIn: true, userName: value });
+  };
+
+  render() {
+    return (
+      <div className="main" id="wrapper">
+        {this.state.isLoggedIn ? (
+          <div>
+            <div className="title">
+              <Text
+                id="main-heading"
+                type="secondary"
+                style={{ fontSize: "36px" }}
+              >
+                Websocket Chat: {this.state.userName}
+              </Text>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                paddingBottom: 50,
+              }}
+              id="messages"
+            >
+              {this.state.messages.map((message) => (
+                <Card
+                  key={message.msg}
+                  style={{
+                    width: 300,
+                    margin: "16px 4px 0 4px",
+                    alignSelf:
+                      this.state.userName === message.user
+                        ? "flex-end"
+                        : "flex-start",
+                  }}
+                  loading={false}
+                >
+                  <Meta
+                    avatar={
+                      <Avatar
+                        style={{ color: "#f56a00", backgroundColor: "#fde3cf" }}
+                      >
+                        {message.user[0].toUpperCase()}
+                      </Avatar>
+                    }
+                    title={message.user + ":"}
+                    description={message.msg}
+                  />
+                </Card>
+              ))}
+            </div>
+            <div className="bottom">
+              <Search
+                placeholder="input message and send"
+                enterButton="Send"
+                value={this.state.searchVal}
+                size="large"
+                onChange={this.handleSearchChange}
+                onSearch={this.handleSearchSubmit}
+              />
+            </div>
+          </div>
+        ) : (
+          <div style={{ padding: "200px 40px" }}>
+            <Search
+              placeholder="Enter Username"
+              enterButton="Login"
+              size="large"
+              onSearch={this.handleLoginSubmit}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default Chat;
-
-
-    
-
-
-// send message from the form
-document.forms.publish.onsubmit = function() {
-  let outgoingMessage = this.message.value;
-
-  socket.send(outgoingMessage);
-  return false;
-};
-
-// message received - show the message in div#messages
-socket.onmessage = function(event) {
-  let message = event.data;
-
-  let messageElem = document.createElement('div');
-  messageElem.textContent = message;
-  document.getElementById('messages').prepend(messageElem);
-}
-
