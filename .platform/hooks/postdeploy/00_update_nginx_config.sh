c5870e916b7586c99fbbc9a5db4972a9e93f599c
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-12-31 20:24:56 -0330

    underscores_in_headers

--- .platform/hooks/postdeploy/00_update_nginx_config.sh
+++ .platform/hooks/postdeploy/00_update_nginx_config.sh
@@ -9,3 +9,6 @@ sudo cp $NGINX_CONF "${NGINX_CONF}.bak"
 # Update types_hash_max_size and server_names_hash_bucket_size
 sudo sed -i '/http {/a \    types_hash_max_size 2048;' $NGINX_CONF
 sudo sed -i '/http {/a \    server_names_hash_bucket_size 128;' $NGINX_CONF
+
+# Update underscores_in_headers on
+sudo sed -i '/http {/a \    underscores_in_headers on;' $NGINX_CONF
