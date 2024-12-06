5ef989526f63272ab4c3ead1bd687f8ff1cd8a75
Soheil Najmabadi Kia <sohnajm@gmail.com>
2024-12-06 13:23:06 -0330

    sosepbackgammon2

--- .platform/hooks/postdeploy/01_get_certificate.sh
+++ .platform/hooks/postdeploy/01_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d sosepbackgammon.ca-central-1.elasticbeanstalk.com --nginx --agree-tos --email sohnajm@gmail.com
+sudo certbot -n -d sosepbackgammon2.ca-central-1.elasticbeanstalk.com --nginx --agree-tos --email sohnajm@gmail.com
